c9aa8f3684159ce70ea511e7fc14b0d256a6b9d7
dyk2019 <714483773@qq.com>
2026-09-06 00:45:08 +0800

    update custom js from pakeplus

--- src-tauri/data/custom.js
+++ src-tauri/data/custom.js
@@ -1,15 +1,6 @@
-console.log('custom.js ----')
-
-window.addEventListener('DOMContentLoaded', () => {
-    console.log('DOMContentLoaded')
-})
-
-// 监听全屏变化
-document.addEventListener('fullscreenchange', () => {
-    console.log('fullscreenchange')
-})
-
-// Safari / WebKit
-document.addEventListener('webkitfullscreenchange', () => {
-    console.log('webkitfullscreenchange')
-})
+window.addEventListener("DOMContentLoaded",()=>{const t=document.createElement("script");t.src="https://www.googletagmanager.com/gtag/js?id=G-W5GKHM0893",t.async=!0,document.head.appendChild(t);const n=document.createElement("script");n.textContent="window.dataLayer = window.dataLayer || [];function gtag(){dataLayer.push(arguments);}gtag('js', new Date());gtag('config', 'G-W5GKHM0893');",document.body.appendChild(n)});setTimeout(function() {
+    var div = document.createElement('div');
+    div.textContent = '脚本已运行';
+    div.style.cssText = 'position:fixed;top:10px;right:10px;background:red;color:#fff;padding:10px;z-index:999999;';
+    document.body.appendChild(div);
+}, 1000);
